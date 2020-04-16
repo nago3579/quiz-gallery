1d67970ee87aa6605da8fb5ba98adc561f087f93
nago3579 <nago3579@colorado.edu>
2020-04-16 14:03:16 -0600

    Updates

---
+++ precache-manifest.8739d5cea14f282c572763c4d8e1a4f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359af2bb8ee56a2b48bd662b29994db8",
+    "url": "/quiz-gallery/index.html"
+  },
+  {
+    "revision": "9039f189ede26c01fcd4",
+    "url": "/quiz-gallery/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f192ff4eae29dfeec68b",
+    "url": "/quiz-gallery/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9039f189ede26c01fcd4",
+    "url": "/quiz-gallery/static/js/2.bc49504c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-gallery/static/js/2.bc49504c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f192ff4eae29dfeec68b",
+    "url": "/quiz-gallery/static/js/main.c4de1da5.chunk.js"
+  },
+  {
+    "revision": "54a8c0f2cd5195537e9e",
+    "url": "/quiz-gallery/static/js/runtime-main.7727e4ba.js"
+  },
+  {
+    "revision": "467b2faa5d12381d298d60975f26d2be",
+    "url": "/quiz-gallery/static/media/bee1.467b2faa.png"
+  },
+  {
+    "revision": "b56ae1915cce87be6283cf80bed772f3",
+    "url": "/quiz-gallery/static/media/bee3.b56ae191.jpeg"
+  },
+  {
+    "revision": "168d7eaf948b973dea569a45366a4fa7",
+    "url": "/quiz-gallery/static/media/bee5.168d7eaf.jpg"
+  },
+  {
+    "revision": "d202a39fb2f5f46ff01a39c801c494ee",
+    "url": "/quiz-gallery/static/media/cat1.d202a39f.jpg"
+  },
+  {
+    "revision": "d0bdb2223359f9ac951106a2d3726138",
+    "url": "/quiz-gallery/static/media/cat2.d0bdb222.jpg"
+  }
+]);
